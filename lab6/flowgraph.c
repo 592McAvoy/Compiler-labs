fa58fc2d1541c55af8b114d67db11e45814f8a62
592McAvoy <1245709462@qq.com>
2018-12-08 22:54:55 +0800

    flowgraph

--- lab6/flowgraph.c
+++ lab6/flowgraph.c
@@ -14,22 +14,134 @@
 #include "errormsg.h"
 #include "table.h"
 
+//helper 
+typedef struct labelNode_ *labelNode;
+struct labelNode_{
+	Temp_label label;
+	AS_targets targets;
+	G_node node;
+};
+labelNode LabelNode( G_node n, Temp_label lab, AS_targets ts){
+	labelNode i = checked_malloc(sizeof(*i));
+	i->label = lab;
+	i->node = n;
+	i->targets = ts;
+	return i;
+}
+
+typedef struct labelNodeList_ *labelNodeList;
+struct labelNodeList_{
+	labelNode head;
+	labelNodeList tail;
+};
+labelNodeList LabelNodeList(labelNode h, labelNodeList t){
+	labelNodeList l = checked_malloc(sizeof(*l));
+	l->head = h;
+	l->tail = t;
+	return l;
+}
+
+
 Temp_tempList FG_def(G_node n) {
 	//your code here.
-	return NULL;
+	AS_instr ins = G_nodeInfo(n);
+	switch(ins->kind){
+		case I_OPER:return ins->u.OPER.dst;
+		case I_LABEL:assert(0);return NULL;
+		case I_MOVE:return ins->u.MOVE.dst;
+	}
 }
 
 Temp_tempList FG_use(G_node n) {
 	//your code here.
-	return NULL;
+	AS_instr ins = G_nodeInfo(n);
+	switch(ins->kind){
+		case I_OPER:return ins->u.OPER.src;
+		case I_LABEL:assert(0);return NULL;
+		case I_MOVE:return ins->u.MOVE.src;
+	}
 }
 
 bool FG_isMove(G_node n) {
 	//your code here.
-	return TRUE;
+	AS_instr ins = G_nodeInfo(n);
+	return ins->kind == I_MOVE;
 }
 
-G_graph FG_AssemFlowGraph(AS_instrList il, F_frame f) {
-	//your code here.
+static labelNodeList labels;
+static labelNodeList waits;
+
+static G_node findLabel(Temp_label label){
+	for(labelNodeList p=labels; p; p=p->tail){
+		Temp_label lab = p->head->label;
+		if(lab == label)
+			return p->head->node;
+	}
 	return NULL;
+}
+static void fillJmp(labelNode ln){
+	Temp_labelList p;
+	for(p = ln->targets->labels; p; p=p->tail){
+		Temp_label lab = p->head;
+		G_node succ = findLabel(lab);
+		if(!succ){
+			EM_impossible("vanishing jump label %s", Temp_labelstring(lab));
+			assert(0);
+		}
+		G_addEdge(ln->node, succ);
+	}
+}
+
+void showInfo(AS_instr ins){
+	printf("%s",ins->u.LABEL.assem);
+}
+G_graph FG_AssemFlowGraph(AS_instrList il) {
+	//your code here.
+	G_graph g = G_Graph();
+	labels = NULL; 
+	waits =NULL;
+
+	bool prevLab = FALSE;
+	Temp_label lab = NULL;
+	G_node prevNode = NULL;
+	//generate flow graph
+	for(AS_instrList ls=il; ls; ls=ls->tail){
+		AS_instr ins = ls->head;
+		switch(ins->kind){
+			case I_LABEL:{
+				prevLab = TRUE;
+				lab = ins->u.LABEL.label; 
+				break;
+			}
+			case I_MOVE:
+			case I_OPER:{
+				G_node node = G_Node(g, ins);
+				if(prevNode){
+					G_addEdge(prevNode, node);
+				}
+				if(prevLab){
+					labels = LabelNodeList(LabelNode(node, lab, NULL),labels);
+					prevLab = FALSE;
+				}
+
+				prevNode = node;
+				if(ins->u.OPER.jumps){
+					char *in = strstr(ins->u.OPER.assem, "jmp");
+					if(in)
+						prevNode = NULL;
+					waits = LabelNodeList(LabelNode(node, NULL, ins->u.OPER.jumps), waits);
+				}
+				break;
+			}
+			default:assert(0);
+		}
+	}
+	//fill jump control
+	for(labelNodeList p=waits;p;p=p->tail){
+		fillJmp(p->head);
+	}
+
+	G_show(stdout, G_nodes(g), showInfo);
+	printf("\n-------====flow graph=====-----\n");
+	return g;
 }
